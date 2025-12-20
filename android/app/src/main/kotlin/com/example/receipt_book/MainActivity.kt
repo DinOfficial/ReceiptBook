18e66bba8f0414b891243c5f36d268c625d105f8
DinOfficial <shdinofficial19@gmail.com>
2025-12-20 20:27:12 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/receipt_book/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.receipt_book
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ReceiptBookApp extends StatefulWidget {
+  const ReceiptBookApp({super.key});
+
+  @override
+  State<ReceiptBookApp> createState() => _ReceiptBookAppState();
+}
+
+class _ReceiptBookAppState extends State<ReceiptBookApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
