cee3632e6d1a8e66f572a422b9bb5d3ae5308d9c
DinOfficial <shdiinofficial19@gmail.com>
2025-12-23 02:05:29 +0300

    homepage completed ui

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:hugeicons/hugeicons.dart';
 import '../widgets/header_home.dart';
+import '../widgets/transection_list_tile.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key, required this.title});
@@ -14,7 +17,14 @@ class HomeScreen extends StatelessWidget {
         body: Column(
           children: [
             HeaderHome(title: title),
-            Expanded(child: Center(child: Text("Below Content"))),
+            Expanded(
+              child: ListView.separated(
+                separatorBuilder: (context, index) => const SizedBox(height: 12),
+                padding: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                itemCount: 20,
+                itemBuilder: (context, index) => TransectionListTile(),
+              ),
+            ),
           ],
         ),
       ),
